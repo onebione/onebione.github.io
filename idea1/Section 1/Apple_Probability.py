92274f7383bf4e648bf3e9fb03a229dd7a41ce77
gtachen <66576663+gtachen@users.noreply.github.com>
2024-06-13 02:13:07 +0000

    Fixed, now should work correctly

--- idea1/Section 1/Apple_Probability.py
+++ idea1/Section 1/Apple_Probability.py
@@ -2,27 +2,27 @@ import math
 import sys
 
 alive_outcomes = 0
+total_outcomes = 0
+
 Apples_Total = int(input("Apples in Total: "))
 Poisonous = int(input("Poisonous Apples: "))
 Apples_To_Kill = int(input("Amount of Poisonous Apples to kill: "))
 Apples_Eat = int(input("Amount of Apples You Will Eat: "))
 
 if Apples_To_Kill > Poisonous or Apples_Eat > Apples_Total:
-    print("You'll be fine ")
+    print("You'll be fine, the apple amounts make no sense")
     sys.exit()
 
 def binomial_coefficients(N, K):
-    if N - K < 0:
+    if K > N:
         return 0
-    result = (math.factorial(N)) // (math.factorial(K) * (math.factorial(N - K)))
-    return result
+    return math.comb(N, K)
     
 for case_num in range(0, Apples_To_Kill):
     var1 = binomial_coefficients(Poisonous, case_num)
     var2 = binomial_coefficients(Apples_Total - Poisonous, Apples_Eat - case_num)
-    var3 = binomial_coefficients(Apples_Total, Apples_Eat)
-    Numerator = (var1) * (var2)
-    Denominator = var3
-    alive_outcomes += (Numerator / Denominator)
+    alive_outcomes += (var1) * (var2)
 
-print(alive_outcomes)
+total_outcomes = binomial_coefficients(Apples_Total, Apples_Eat)
+
+print(str((alive_outcomes / total_outcomes) * 100) + "%")
