65dd3625d04fb2e13660ae7de82066a2f710e4cf
gtachen <66576663+gtachen@users.noreply.github.com>
2024-07-17 18:26:11 +0000

    with help button how to play

--- idea1/Section 3/Image_scripts.js
+++ idea1/Section 3/Image_scripts.js
@@ -192,3 +192,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const submitButton = document.querySelector('.SubmitButton');
     submitButton.disabled = true;
 });
+
+var modal = document.getElementById("Help-Page");
+
+// Get the button that opens the modal
+var btn = document.getElementById("HelpBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeBtn")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
