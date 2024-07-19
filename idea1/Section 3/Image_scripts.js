280753d683ed67db4a946f3b0f62acf7466eb601
gtachen <66576663+gtachen@users.noreply.github.com>
2024-07-19 22:47:46 +0000

    New sentence!

--- idea1/Section 3/Image_scripts.js
+++ idea1/Section 3/Image_scripts.js
@@ -1,4 +1,4 @@
-const sentence = "A buff man with a trident is fighting a shark";
+const sentence = " The beach is crowded on hot days";
 
 function createElementsFromSentence(sentence) {
     const container = document.getElementById('textarea-container');
