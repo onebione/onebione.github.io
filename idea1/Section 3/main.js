16543d69d8cbf409080812fdc7da6275b58a061a
gtachen <66576663+gtachen@users.noreply.github.com>
2024-07-25 18:07:15 +0000

    new sentence!!

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "The monkeys casually walked away from the erupting volcano";
+const sentence = "twin dragons blew fire all over a city";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
