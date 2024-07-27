6c056f003121a09f0ef88499d246ed263a1f4eaf
gtachen <66576663+gtachen@users.noreply.github.com>
2024-07-27 23:50:24 +0000

    long one

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "An explorer found a ghost town in the forest";
+const sentence = "A Colossal squid emerged from the ocean and wrapped its tentacles around a ship during a storm";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
