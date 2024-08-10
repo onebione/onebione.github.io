bf1d807fce779ddc2b5752ed78ca558dcebfc3bd
gtachen <66576663+gtachen@users.noreply.github.com>
2024-08-10 21:57:47 +0000

    hdvhdghjdfgvjhhgvjggvjmgjhx

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = " A meteor shower destroyed the city";
+const sentence = "A demon attacked a girl in the dark forest as she tried to run away in fear";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
