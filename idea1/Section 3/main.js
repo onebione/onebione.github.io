cd6a914b7835251f63f35d031719ef7e2a9f4e3a
onebione <43618220+onebione@users.noreply.github.com>
2024-08-18 19:25:21 +0000

    remove some pic and add new sentence

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "A boy genius terrorizes his family";
+const sentence = "Deep in the forest a seedling of evil grows";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
