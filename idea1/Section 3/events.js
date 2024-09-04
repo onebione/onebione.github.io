74a0f7b76f1bbac7e12461e1fd5bc2dec74be926
onebione <43618220+onebione@users.noreply.github.com>
2024-09-04 21:39:10 +0000

    more hints

--- idea1/Section 3/events.js
+++ idea1/Section 3/events.js
@@ -72,7 +72,7 @@ export function handicapFreebie() {
         const charIndex = textarea.dataset.charIndex;
         const x = sentence.split(' ')[wordIndex][charIndex];
         const y = x.toUpperCase();
-        if ( y == 'A' || y == 'E' || y == 'S')
+        if ( y == 'A' || y == 'E' || y == 'S' || y == 'O' || y == 'N')
         {
           textarea.value = x;
           textarea.classList.add('valid-input');
