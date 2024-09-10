8edc579cc7d42173878ec0c36694484b111524e2
onebione <43618220+onebione@users.noreply.github.com>
2024-09-10 18:03:19 +0000

    new sentence

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "an illuminated book of magic shows a world far away";
+const sentence = "A man in a grassy field gaze up at the starry Milky Way";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
