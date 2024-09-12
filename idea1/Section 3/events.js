f068303eae73aa2d2266fa56c62b1255913ebfc5
onebione <43618220+onebione@users.noreply.github.com>
2024-09-12 21:28:00 +0000

    no A, try O

--- idea1/Section 3/events.js
+++ idea1/Section 3/events.js
@@ -72,7 +72,7 @@ export function handicapFreebie() {
         const charIndex = textarea.dataset.charIndex;
         const x = sentence.split(' ')[wordIndex][charIndex];
         const y = x.toUpperCase();
-        if ( y == 'E' || y == 'T' || y == 'A' )
+        if ( y == 'E' || y == 'T' || y == 'O' )
         {
           textarea.value = x;
           textarea.classList.add('valid-input');
