10534ade25c086edbc3538303bca1b045055e97b
onebione <43618220+onebione@users.noreply.github.com>
2024-09-14 19:53:55 +0000

    chunky

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "obese pikachu is strangely jolly but he is sure to suffer in battle";
+const sentence = "chunky pikachu is strangely jolly but he is sure to suffer in battle";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
