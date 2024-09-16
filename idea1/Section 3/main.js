0a7eb195c943896412721f2e761706a856e7e739
onebione <43618220+onebione@users.noreply.github.com>
2024-09-16 20:23:44 +0000

    galleon sentence

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "this is the cute rainbow colored cthulhu people have been seeing deep in the swamp";
+const sentence = "the galleon ship is definitely stuck on the beautiful tropical beach";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
