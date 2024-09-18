51cd69c2ba457701e7826fe569263dc092d8a012
onebione <43618220+onebione@users.noreply.github.com>
2024-09-18 18:15:58 +0000

    lighthouse

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "a brutally modern architecture serves as a symbol of human hubris";
+const sentence = "a futuristic scene of lighthouse rockets blasting off towards the cosmos";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
