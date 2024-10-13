720c1387151f0747b8869420c1acb00998c3019b
onebione <43618220+onebione@users.noreply.github.com>
2024-10-13 23:35:53 +0000

    sand

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "Halloween is the only night she puts on a smile and comes out of the shadow";
+const sentence = "butterflies flutter into stardust as the sand of time never stops in the eternal hourglass";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
