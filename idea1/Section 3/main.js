90dd69c76b3abcd1227b1459414a67d683dff121
onebione <43618220+onebione@users.noreply.github.com>
2024-10-16 00:42:17 +0000

    gandolf

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "sweet honey drips down on the new queen as buzzing worker bees delight in the new era";
+const sentence = "gangster gandolf gives balrog hell using the juiciest spell of them all";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
