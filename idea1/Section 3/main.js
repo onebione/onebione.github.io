aaadfec20660551bc07fb3e974d6714ade553d90
onebione <43618220+onebione@users.noreply.github.com>
2024-10-16 19:01:02 +0000

    book

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "gangster gandolf gives balrog hell using the juiciest spell of them all";
+const sentence = "a book can transport you to a fantastic new world full of adventure";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
