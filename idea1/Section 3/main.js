58251f5d2ffc85d91864c12eb5b746c8ade60c93
onebione <43618220+onebione@users.noreply.github.com>
2024-10-28 21:16:18 +0000

    better wording

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "the eye of the owl is ever watchful, watching your every move at night and wondering why you stay up so late";
+const sentence = "the eye of the owl is ever watchful, judging your every move and wondering why you stay up so late";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
