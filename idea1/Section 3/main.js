db0c35b8f0db13428d15686f442bf6fcb73fae02
onebione <43618220+onebione@users.noreply.github.com>
2024-10-29 21:27:51 +0000

    sentence change

--- idea1/Section 3/main.js
+++ idea1/Section 3/main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "so I was walking in the woods when this carnivorous plant took a bite out of the vegetarian in front of me...";
+const sentence = "so I was walking in the woods when this carnivorous plant took a bite out of the vegetarian walking ahead of me...";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
