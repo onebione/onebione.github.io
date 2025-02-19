6cb1c44008aec1821955c27aa9d976d8056511d7
onebione <43618220+onebione@users.noreply.github.com>
2025-02-19 22:39:35 +0000

    broccoli 2

--- events.js
+++ events.js
@@ -72,7 +72,7 @@ export function handicapFreebie() {
         const charIndex = textarea.dataset.charIndex;
         const x = sentence.split(' ')[wordIndex][charIndex];
         const y = x.toUpperCase();
-        if ( y == '!' || y == ',' || y == '.' || y == '\'' || y == '?' || y == 'S' || y == 'A' || y == 'L' || y == 'J')
+        if ( y == '!' || y == ',' || y == '.' || y == '\'' || y == '?' || y == 'S' || y == 'A' || y == 'E' || y == 'T')
         {
           textarea.value = x;
           textarea.classList.add('valid-input');
