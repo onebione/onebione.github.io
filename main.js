d5d74d25ae1ace9bc9c49428bb3a345a7405263d
onebione <43618220+onebione@users.noreply.github.com>
2025-02-19 22:32:18 +0000

    broccoli

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "The juiciest male ever set his heart ablaze";
+const sentence = "Broccoli, that glorious green champion of the vegetable kingdom, is so undeniably delicious and packed with vitamins that I'd happily devour a mountain of it, even if it meant I would be crushed under all its weight!!";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
