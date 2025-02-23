fe3e30a8125da18dc41dc1ac62029d78d237dd21
onebione <43618220+onebione@users.noreply.github.com>
2025-02-23 01:18:37 +0000

    holomask

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "Ethereal fungi, crystalline blooms, a symbiotic dance of nature's artistry, where earth and mineral intertwine in breathtaking beauty";
+const sentence = "In the surreal desert, a halo knight, hooded and glowing-eyed, stalked through shimmering heatwaves, its cybernetic form adorned with an iridescent, crystalline mask projecting a holographic, dreamlike visage";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
