c01b5584668dfa40ced0603caa7b7d1f3e54c82a
onebione <43618220+onebione@users.noreply.github.com>
2025-02-23 03:00:10 +0000

    all vowels

--- events.js
+++ events.js
@@ -72,7 +72,7 @@ export function handicapFreebie() {
         const charIndex = textarea.dataset.charIndex;
         const x = sentence.split(' ')[wordIndex][charIndex];
         const y = x.toUpperCase();
-        if ( y == '!' || y == '-' || y == ',' || y == '.' || y == '\'' || y == '?' || y == 'E' || y == 'T' || y == 'A' || y == 'O' || y == 'W' || y == 'Z')
+        if ( y == '!' || y == '-' || y == ',' || y == '.' || y == '\'' || y == '?' || y == 'E' || y == 'T' || y == 'A' || y == 'O' || y == 'I' || y == 'U')
         {
           textarea.value = x;
           textarea.classList.add('valid-input');
