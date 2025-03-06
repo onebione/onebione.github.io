fab5213c5361ae47aaf52d27d43af40bc8e87769
onebione <43618220+onebione@users.noreply.github.com>
2025-03-06 01:26:27 +0000

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "Super precious cute cat girl in my class holds a caricature of herself";
+const sentence = "Super precious chibi cat girl in my class holds a caricature of herself";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
