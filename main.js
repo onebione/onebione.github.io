1608c7af7f2925a9456e190b2757c9695e3423a4
onebione <43618220+onebione@users.noreply.github.com>
2025-03-21 00:08:19 +0000

    magic mouse

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "Super precious chibi cat girl in my class holds a caricature of herself";
+const sentence = "The mouse in a coral flower field, a tiny vessel of enchantment, bore swirling patterns of pure gold that shimmered across its fur";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
