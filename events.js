d71aaadb6fc5cc6e780faccc09a4769ec0dcfd57
onebione <43618220+onebione@users.noreply.github.com>
2025-03-30 01:09:32 +0000

    ghibli cat

--- events.js
+++ events.js
@@ -72,7 +72,7 @@ export function handicapFreebie() {
         const charIndex = textarea.dataset.charIndex;
         const x = sentence.split(' ')[wordIndex][charIndex];
         const y = x.toUpperCase();
-        if ( y == '!' || y == '-' || y == ',' || y == '.' || y == '\'' || y == '?' || y == 'E' || y == 'T' || y == 'A' || y == 'O' || y == 'U' || y == 'M')
+        if ( y == '!' || y == '-' || y == ',' || y == '.' || y == '\'' || y == '?' || y == 'E' || y == 'T' || y == 'A' || y == 'O' || y == 'C' || y == 'M')
         {
           textarea.value = x;
           textarea.classList.add('valid-input');
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "The mouse in a coral flower field, a tiny vessel of enchantment, bore swirling patterns of pure gold that shimmered across its fur";
+const sentence = "A dark fairytale, in the style of Studio Ghibli, shows a serene yet eerie forest where a large, black creature stands, with a white cat and delicate flowers.";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
