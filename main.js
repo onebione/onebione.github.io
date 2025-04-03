5e794795e918518446c1433e6655ee3b1b1d310b
onebione <43618220+onebione@users.noreply.github.com>
2025-04-03 01:34:24 +0000

    mechbug

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "A miniature diorama shows people climbing a towering stack of cheese blocks, presented on a wooden board in the office";
+const sentence = "A jeweled, metallic insect, with cosmic eyes, stands poised, a mechanical deity ready to unleash its intricate power upon reality";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
