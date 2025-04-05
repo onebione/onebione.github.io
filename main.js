2bcf913bfcb0a5dfd87a24ca97357c3339dabc65
onebione <43618220+onebione@users.noreply.github.com>
2025-04-05 05:25:44 +0000

    crossdress

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "A jeweled, metallic insect, with cosmic eyes, stands poised, a mechanical deity ready to unleash its intricate power upon reality";
+const sentence = "The gigachad, despite his muscular physique and chiseled chin, looked surprisingly comfortable in the maid dress, a testament to his confident embrace of cross-dressing";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
