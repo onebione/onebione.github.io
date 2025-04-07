3c85ebc818bf31c16689bf3158996c115cfa7d46
onebione <43618220+onebione@users.noreply.github.com>
2025-04-07 21:41:10 +0000

    cat

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "A decaying, fat, evil clown in a colorful top hat grins menacingly in a vibrant, dark fantasy";
+const sentence = "A cute, giddy furry anthropomorphic cat with pink hair and purple eyes waves from a pastel flower field, blushing and smiling adorably";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
