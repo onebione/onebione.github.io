b1afa5543c82d4a779abf82ed6e29f79be585080
onebione <43618220+onebione@users.noreply.github.com>
2025-04-11 05:47:05 +0000

    sand

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "An enchanting paper landscape captures an ancient setting with a watermill, houses, warm tones, and a blurred, golden, inviting space";
+const sentence = "Witness the majestic, awesome showdown as ancient colossal mummy fights warrior of the light amidst chaotic sands";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
