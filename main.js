c49a082fba82ab7895ace469a391b6ca4347f358
onebione <43618220+onebione@users.noreply.github.com>
2025-04-18 22:54:04 +0000

    sorcerer

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "Witness the majestic, awesome showdown as ancient colossal mummy fights warrior of the light amidst chaotic sands";
+const sentence = "In a forgotten castle chamber, a translucent sorcerer clutches a staff whose swirling orb exuded palpable malevolence";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
