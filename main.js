c1fc1b8a15a1eae0be67f793b69561c27d8664be
onebione <43618220+onebione@users.noreply.github.com>
2025-04-24 01:50:29 +0000

    sunflower

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "A giant tower of ham, cheese, lettuce, and tomato arose as construction workers directed cranes and bulldozers on the site";
+const sentence = "The pocket sized dragon knight, gripping his lance, faces off against the colossal furious sunflower bloom";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
