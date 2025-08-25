cb1e49c8289175ca6d3cf2832edc0e800636a0b1
onebione <43618220+onebione@users.noreply.github.com>
2025-08-25 01:56:11 +0000

    Grogu

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "In the twilight haze, her glowing eyes whispered ancient horrors too unholy to speak";
+const sentence = "Chaos surrounds, yet Grogu savors his meal peacefully, embodying calm resilience in the midst of destruction.";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
