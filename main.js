fdc980ae5a42f0ab8473787dac48d5c797a23578
onebione <43618220+onebione@users.noreply.github.com>
2025-09-12 02:06:12 +0000

    apostrophe

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "When you accidentally time-travel to the Renaissance but still manage to look like you’re about to star in John Wick 5: The Da Vinci Code";
+const sentence = "When you accidentally time-travel to the Renaissance but still manage to look like you're about to star in John Wick 5: The Da Vinci Code";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
