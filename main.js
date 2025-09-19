a3e47c81100f398bae6da3001510e7168249abfe
onebione <43618220+onebione@users.noreply.github.com>
2025-09-19 01:30:54 +0000

    trumpstein

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "Instagram's true form revealed: a many-eyed beast that devours your time, attention, and mental health";
+const sentence = "Trump's frightened expression is a symbol of his inescapable guilt from association with the depravity of the now eternally damned Epstein";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
