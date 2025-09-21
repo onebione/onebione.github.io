e892dcdb605aefe69b2937af7d6cc25bbf4e6a65
onebione <43618220+onebione@users.noreply.github.com>
2025-09-21 23:51:38 +0000

    dumpings

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createElementsFromSentence, resetSentence } from './elements.js';
 import { addEventListeners, handicapFreebie } from './events.js';
 import { initializeModal } from './modal.js';
 
-const sentence = "Trump's frightened expression is a symbol of his inescapable guilt from association with the depravity of the now eternally damned Epstein";
+const sentence = "a chubby soup dumpling creature is found spilling out its juicy savory fillings and trying to eat his share";
 
 document.addEventListener('DOMContentLoaded', () => {
     createElementsFromSentence(sentence);
